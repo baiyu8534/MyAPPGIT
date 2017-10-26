ace827f1c1f8d3fab8c541c2b745be97be33b90c
baiyu8534 <657116885@qq.com>
2017-10-26 17:49:10 +0800

    BublesSwitchView优化： 小球圆心偏移了一点，看起来更舒服些 加了监听事件，onCheckChangeListenet,onClickListener, setSelect 小球颜色随小球移动而在两个颜色之间渐变，速度和小球速度一样也是，两个颜色可以手动设置 背景颜色随小球移动而在两个颜色之间渐变，速度和小球速度一样也是，两个颜色可以手动设置 用户可以设置自己的插值器 小球速度的动态设置（现在是判断控件长度而去设置的，代码写死的。。待优化）
    待优化：
    自定义插值器（一个默认的，看着舒服的）
    设置笔的渲染图片（背景为两个图片的渐变）
    小球速度的动态设置（和控件大小有关现在是判断控件长度而去设置的，代码写死的。。待优化）

--- app/src/main/java/com/example/administrator/myappgit/activity/BublesSwitchViewActivity.java
+++ app/src/main/java/com/example/administrator/myappgit/activity/BublesSwitchViewActivity.java
@@ -2,8 +2,19 @@ package com.example.administrator.myappgit.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.example.administrator.myappgit.R;
+import com.example.administrator.myappgit.ui.BublesSwitchView;
+import com.example.administrator.myappgit.utils.UIUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 文件名：BublesSwitchViewActivity
@@ -13,11 +24,121 @@ import com.example.administrator.myappgit.R;
  * 版权：
  */
 
-public class BublesSwitchViewActivity extends BaseActivity {
+public class BublesSwitchViewActivity extends BaseActivity implements View.OnClickListener, BublesSwitchView
+        .OnCheckedChangeListener {
+
+    @BindView(R.id.bsw_40)
+    BublesSwitchView mBsw40;
+    @BindView(R.id.bsw_50)
+    BublesSwitchView mBsw50;
+    @BindView(R.id.bsw_60)
+    BublesSwitchView mBsw60;
+    @BindView(R.id.bsw_100)
+    BublesSwitchView mBsw100;
+    @BindView(R.id.bsw_150)
+    BublesSwitchView mBsw150;
+    @BindView(R.id.bsw_200)
+    BublesSwitchView mBsw200;
+    @BindView(R.id.bsw_250)
+    BublesSwitchView mBsw250;
+    @BindView(R.id.bsw_300)
+    BublesSwitchView mBsw300;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubles_switchview_layout);
+        ButterKnife.bind(this);
+
+        initView();
+        initListener();
+
+    }
+
+    private void initView() {
+        mBsw40.setSelect(true);
+        mBsw50.setSelect(false);
+        mBsw60.setSelect(true);
+        mBsw100.setSelect(false);
+        mBsw150.setSelect(true);
+        mBsw200.setSelect(false);
+        mBsw250.setSelect(true);
+        mBsw300.setSelect(false);
+
+//        mBsw40.setInterpolator();
+//        mBsw50.setInterpolator();
+//        mBsw60.setInterpolator();
+        mBsw100.setInterpolator(new AccelerateDecelerateInterpolator());
+        mBsw150.setInterpolator(new AnticipateInterpolator());
+        mBsw200.setInterpolator(new AccelerateInterpolator());
+        mBsw250.setInterpolator(new BounceInterpolator());
+        mBsw300.setInterpolator(new DecelerateInterpolator());
+    }
+
+    private void initListener() {
+        mBsw300.setOnClickListener(this);
+        mBsw300.setOnCheckedChangeListener(this);
+    }
+
+    int i = 0;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bsw_40:
+                UIUtil.toastShort(mContext,"bsw_40 被点击了");
+                break;
+            case R.id.bsw_50:
+                UIUtil.toastShort(mContext,"bsw_50 被点击了");
+                break;
+            case R.id.bsw_60:
+                UIUtil.toastShort(mContext,"bsw_60 被点击了");
+                break;
+            case R.id.bsw_100:
+                UIUtil.toastShort(mContext,"bsw_100 被点击了");
+                break;
+            case R.id.bsw_150:
+                UIUtil.toastShort(mContext,"bsw_150 被点击了");
+                break;
+            case R.id.bsw_200:
+                UIUtil.toastShort(mContext,"bsw_200 被点击了");
+                break;
+            case R.id.bsw_250:
+                UIUtil.toastShort(mContext,"bsw_250 被点击了");
+                break;
+            case R.id.bsw_300:
+                UIUtil.toastShort(mContext,"bsw_300 被点击了"+ ++i +"次");
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChange(BublesSwitchView bublesSwitchView, boolean isSelected) {
+        switch (bublesSwitchView.getId()){
+            case R.id.bsw_40:
+                UIUtil.toastShort(mContext,"bsw_40 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_50:
+                UIUtil.toastShort(mContext,"bsw_50 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_60:
+                UIUtil.toastShort(mContext,"bsw_60 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_100:
+                UIUtil.toastShort(mContext,"bsw_100 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_150:
+                UIUtil.toastShort(mContext,"bsw_150 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_200:
+                UIUtil.toastShort(mContext,"bsw_200 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_250:
+                UIUtil.toastShort(mContext,"bsw_250 的选中状态变为"+isSelected);
+                break;
+            case R.id.bsw_300:
+                UIUtil.toastShort(mContext,"bsw_300 的选中状态变为"+isSelected);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/administrator/myappgit/ui/BublesSwitchView.java
+++ app/src/main/java/com/example/administrator/myappgit/ui/BublesSwitchView.java
@@ -1,6 +1,7 @@
 package com.example.administrator.myappgit.ui;
 
 import android.animation.Animator;
+import android.animation.ArgbEvaluator;
 import android.animation.FloatEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Context;
@@ -13,6 +14,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
 
 import com.example.administrator.myappgit.BuildConfig;
 
@@ -25,8 +28,12 @@ import com.example.administrator.myappgit.BuildConfig;
  */
 
 public class BublesSwitchView extends View {
-    // TODO: 2017/10/25 0025 还可以优化下，比如自定义插值器，设置笔的渲染图片，小球速度的动态设置（和控件大小有关）
+    // TODO: 2017/10/25 0025 还可以优化下，比如自定义插值器，设置笔的渲染图片
+    // FIXME: 2017/10/26 0026 每种状态的 小球颜色 BG颜色的设置方法还没加
 
+    //c开头是小球相关属性  s开头是背景田径场属性  m开头是控件View的属性
+
+    private Context mContext;
     private final Paint paint = new Paint();
     /**
      * 田径场的path
@@ -46,11 +53,16 @@ public class BublesSwitchView extends View {
     private float halfHeight; //一半高度
 
     private float cRadius; //圆的半径
-    private float cPercent = 0.7f; //圆半径 相对于f一半高度的百分比
-    private float spacePercent = 0.3f; //圆与边框距离 相对于一半高度的百分比
+    private float cPercent; //圆半径 相对于f一半高度的百分比
+    private float spacePercent; //圆与边框距离 相对于一半高度的百分比
     // F false T true ....
     private float cFCenterX, cCenterY; //圆心坐标 未选中状态
     private float cTCenterX; //圆心坐标 选中状态
+
+    /**
+     * 尾巴的RectF
+     */
+    RectF oval;
 
     /**
      * 未选中状态 尾巴（椭圆)的左上右下
@@ -82,29 +94,125 @@ public class BublesSwitchView extends View {
     private ValueAnimator mValueAnimator;
 
     /**
-     * 数值动画变化的百分比 0 --> 1
+     * 数值动画变化的百分比 0 --> 1 （没加插值器，就是平稳变化）
      */
     private float mAnimatedFraction;
 
     /**
-     * 动画执行时间
+     * 动画执行时间 随控件大小改变
      */
-    private long time = 500;
+    private long time;
     /**
      * 动画执行的当前时间
      */
     private long mCurrentPlayTime;
 
+    /**
+     * 小球移动速度 用速度也不好搞。。
+     */
+    //private float speed = 700;
+
+    /**
+     * 不知道怎么形容的比例。。。
+     */
+    private float xPercent = 80;
+
+    /**
+     * 圆心偏移距离
+     */
+    private float cCenterOffset;
+
+    /**
+     * 小球移动速度的插值器
+     */
+    private Interpolator mInterpolator;
+
+    /**
+     * 小球移动时颜色改变的插值器
+     */
+    private ArgbEvaluator mArgbEvaluator;
+
+    /**
+     * 选中状态小球的颜色 ARGB格式
+     */
+    private int cSelectColer;
+
+    /**
+     * 未选中状态小球的颜色
+     */
+    private int cUnSelectColer;
+
+    /**
+     * 选中状态 背景 的颜色 ARGB格式
+     */
+    private int sSelectColer;
+
+    /**
+     * 未选中状态 背景 的颜色
+     */
+    private int sUnSelectColer;
+
+    /**
+     * ---------------------------
+     **/
+
+    public interface OnCheckedChangeListener {
+        void onCheckedChange(BublesSwitchView bublesSwitchView, boolean isSelected);
+    }
+
+    private OnClickListener mOnClickListener;
+
+    private OnCheckedChangeListener mOnCheckedChangeListener;
+
+    public boolean isSelect() {
+        return select;
+    }
+
+    public void setSelect(boolean select) {
+        this.select = select;
+    }
+
+    @Override
+    public void setOnClickListener(OnClickListener onClickListener) {
+        mOnClickListener = onClickListener;
+    }
+
+    /**
+     * 监听选中状态的改变
+     *
+     * @param onCheckedChangeListener
+     */
+    public void setOnCheckedChangeListener(OnCheckedChangeListener onCheckedChangeListener) {
+        mOnCheckedChangeListener = onCheckedChangeListener;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+    }
+
+    /**
+     * 设置小球运动速度的插值器
+     *
+     * @param interpolator
+     */
+    public void setInterpolator(Interpolator interpolator) {
+        this.mInterpolator = interpolator;
+    }
+
     public BublesSwitchView(Context context) {
         super(context);
+        mContext = context;
     }
 
     public BublesSwitchView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
     }
 
     public BublesSwitchView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mContext = context;
     }
 
     @Override
@@ -114,90 +222,130 @@ public class BublesSwitchView extends View {
         setMeasuredDimension(width, height);
     }
 
-
     @Override
     protected void onDraw(final Canvas canvas) {
         super.onDraw(canvas);
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(0xff000000);
-        canvas.drawPath(sPath, paint); // 画出田径场
-        canvas.save();
-        paint.reset();
-
-        //画圆和后面的尾巴
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(0xffffffff);
-
 
         //因为是实时绘制，用数值动画，开始时刷新绘制要先画一个圆，要不会闪一下
         if (animIsStart && moveX == -1) {
+
             //开始（点击后，动画启动）
             mValueAnimator.start();
             if (!select) {
-                canvas.drawCircle(cFCenterX, cCenterY, cRadius, paint);
+                drawStaticBG(canvas, sUnSelectColer);
+                drowStaticCircle(canvas, cUnSelectColer, cFCenterX, cCenterY);
             } else {
-                canvas.drawCircle(cTCenterX, cCenterY, cRadius, paint);
+                drawStaticBG(canvas, sSelectColer);
+                drowStaticCircle(canvas, cSelectColer, cTCenterX, cCenterY);
             }
         } else if (animIsStart) {
+
             //过程中（圆移动过程中）
             if (!select) {
+
+                drawStaticBG(canvas, (Integer) mArgbEvaluator.evaluate(mAnimatedFraction, sUnSelectColer, sSelectColer));
+
+                paint.setAntiAlias(true);
+                paint.setStyle(Paint.Style.FILL);
+                paint.setColor((Integer) mArgbEvaluator.evaluate(mAnimatedFraction, cUnSelectColer, cSelectColer));
                 canvas.drawCircle(cFCenterX + moveX, cCenterY, cRadius, paint);
                 canvas.save();
-                RectF oval;
                 //实际移动距离过一半后尾巴要缩小
                 //因为不管控件多大（即小球走过的距离不定），但是动画执行时间一样，导致控件越大，小球看起来走的越快
-                //mAnimatedFraction * mWidth / (time / 100) 需要和小球移动的速度有关，即速度越大，尾巴越长（速度：mWidth / (time / 100) 。time /
-                // 100是让数值变大，尾巴明显）
+                //mAnimatedFraction * mWidth / (time / 100) 需要和小球移动的速度有关，即速度越大，尾巴越长 速度：mWidth / (time / 100)
+                //time / 100是让数值变大，尾巴明显
                 if (moveX < (mWidth - mHeight) / 2) {
-                    oval = new RectF(
-                            oFLeft + moveX - mAnimatedFraction * mWidth / (time / 100),
-                            oFTop,
-                            oFRight + moveX + mAnimatedFraction * mWidth / (time / 100),
-                            oFBotton);
+                    oval.left = oFLeft + moveX - mAnimatedFraction * mWidth / (time / xPercent);
+                    oval.top = oFTop;
+                    oval.right = oFRight + moveX + mAnimatedFraction * mWidth / (time / xPercent);
+                    oval.bottom = oFBotton;
                 } else {
-                    oval = new RectF(
-                            oFLeft + moveX - (mAnimatedFraction * mWidth / (time / 100)) + 2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / 100)),
-                            oFTop,
-                            oFRight + moveX + (mAnimatedFraction * mWidth / (time / 100)) - 2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / 100)),
-                            oFBotton);
+                    oval.left = oFLeft + moveX - (mAnimatedFraction * mWidth / (time / xPercent)) +
+                            2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / xPercent));
+                    oval.top = oFTop;
+                    oval.right = oFRight + moveX + (mAnimatedFraction * mWidth / (time / xPercent)) -
+                            2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / xPercent));
+                    oval.bottom = oFBotton;
                 }
-                canvas.drawArc(oval, 90, 180, false, paint);//小弧形
+                canvas.drawArc(oval, 90, 180, false, paint);
+                canvas.save();
+                paint.reset();
             } else {
+
+                drawStaticBG(canvas, (Integer) mArgbEvaluator.evaluate(mAnimatedFraction, sSelectColer, sUnSelectColer));
+
+                paint.setAntiAlias(true);
+                paint.setStyle(Paint.Style.FILL);
+                paint.setColor((Integer) mArgbEvaluator.evaluate(mAnimatedFraction, cSelectColer, cUnSelectColer));
                 canvas.drawCircle(cTCenterX - moveX, cCenterY, cRadius, paint);
                 canvas.save();
-                RectF oval;
                 //实际移动距离过一半后尾巴要缩小
                 if (moveX < (mWidth - mHeight) / 2) {
-                    oval = new RectF(
-                            oTLeft - moveX - mAnimatedFraction * mWidth / (time / 100),
-                            oTTop,
-                            oTRight - moveX + mAnimatedFraction * mWidth / (time / 100),
-                            oTBotton);
+                    oval.left = oTLeft - moveX - mAnimatedFraction * mWidth / (time / xPercent);
+                    oval.top = oTTop;
+                    oval.right = oTRight - moveX + mAnimatedFraction * mWidth / (time / xPercent);
+                    oval.bottom = oTBotton;
                 } else {
-                    oval = new RectF(
-                            oTLeft - moveX - (mAnimatedFraction * mWidth / (time / 100)) + 2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / 100)),
-                            oTTop,
-                            oTRight - moveX + (mAnimatedFraction * mWidth / (time / 100)) - 2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / 100)),
-                            oTBotton);
+                    oval.left = oTLeft - moveX - (mAnimatedFraction * mWidth / (time / xPercent)) +
+                            2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / xPercent));
+                    oval.top = oTTop;
+                    oval.right = oTRight - moveX + (mAnimatedFraction * mWidth / (time / xPercent)) -
+                            2 * ((mAnimatedFraction - 0.5f) * mWidth / (time / xPercent));
+                    oval.bottom = oTBotton;
                 }
-                canvas.drawArc(oval, 90, -180, false, paint);//小弧形
+                canvas.drawArc(oval, 90, -180, false, paint);
+                canvas.save();
+                paint.reset();
             }
         } else {
             //结束 （动画结束后）
             if (!select) {
-                canvas.drawCircle(sHeight / 2, sHeight / 2, sHeight / 2 * 0.7f, paint);
+                drawStaticBG(canvas, sUnSelectColer);
+                drowStaticCircle(canvas, cUnSelectColer, cFCenterX, cCenterY);
             } else {
-                canvas.drawCircle(sWidth - sHeight / 2, sHeight / 2, sHeight / 2 * 0.7f, paint);
+                drawStaticBG(canvas, sSelectColer);
+                drowStaticCircle(canvas, cSelectColer, cTCenterX, cCenterY);
             }
         }
         canvas.restore();
     }
 
+    /**
+     * 画背景田径场
+     *
+     * @param canvas
+     * @param colorInt ARGB 0xAARRGGBB
+     */
+    private void drawStaticBG(Canvas canvas, int colorInt) {
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(colorInt);
+        canvas.drawPath(sPath, paint); // 画出田径场
+        canvas.save();
+        paint.reset();
+    }
+
+    /**
+     * 画静态的小球
+     *
+     * @param canvas
+     * @param colorInt ARGB 0xAARRGGBB
+     */
+    private void drowStaticCircle(Canvas canvas, int colorInt, float centerX, float centerY) {
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(colorInt);
+        canvas.drawCircle(centerX, centerY, cRadius, paint);
+        canvas.save();
+        paint.reset();
+    }
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+
+        oval = new RectF();
+        mArgbEvaluator = new ArgbEvaluator();
 
         //初始化数值
         mWidth = w; // 视图自身宽度
@@ -211,20 +359,44 @@ public class BublesSwitchView extends View {
 //        sCenterY = (sBottom + sTop) / 2; // 田径场的Y轴中心坐标
 
         halfHeight = sHeight / 2;
+//        time = (long) (speed * 100f / mWidth * 4);
+        cCenterOffset = halfHeight * 0.16f;
+        cPercent = 0.64f;
+        spacePercent = 1f - cPercent;
 
-        cFCenterX = halfHeight;
-        cTCenterX = sWidth - halfHeight;
+        //color  0xff000000黑色
+        cSelectColer = 0xffE91E63;
+        cUnSelectColer = 0xff5D646E;
+
+        sSelectColer = 0xff00796B;
+        sUnSelectColer = 0xff000000;
+
+        //动画执行时间
+        if (mWidth <= 200) {
+            time = 450;
+        } else if (mWidth <= 300) {
+            time = 500;
+        } else if (mWidth <= 500) {
+            time = 550;
+        } else if (mWidth <= 700) {
+            time = 600;
+        } else {
+            time = 700;
+        }
+
+        cFCenterX = halfHeight - cCenterOffset;
+        cTCenterX = sWidth - halfHeight + cCenterOffset;
         cCenterY = halfHeight;
         cRadius = halfHeight * cPercent;
 
-        oFLeft = halfHeight * spacePercent;
+        oFLeft = halfHeight * spacePercent - cCenterOffset;
         oFTop = halfHeight * spacePercent;
-        oFRight = sHeight - halfHeight * spacePercent;
+        oFRight = sHeight - halfHeight * spacePercent - cCenterOffset;
         oFBotton = sHeight - halfHeight * spacePercent;
 
-        oTLeft = sWidth - sHeight + halfHeight * spacePercent;
+        oTLeft = sWidth - sHeight + halfHeight * spacePercent + cCenterOffset;
         oTTop = halfHeight * spacePercent;
-        oTRight = sWidth - halfHeight * spacePercent;
+        oTRight = sWidth - halfHeight * spacePercent + cCenterOffset;
         oTBotton = sHeight - halfHeight * spacePercent;
 
 
@@ -236,10 +408,11 @@ public class BublesSwitchView extends View {
         sPath.close();    // path准备田径场的路径
 
 
-        mValueAnimator = ValueAnimator.ofObject(new FloatEvaluator(), 0, (float) (mWidth - mHeight));
+        mValueAnimator = ValueAnimator.ofObject(new FloatEvaluator(), 0, cTCenterX - cFCenterX);
         // TODO: 2017/10/25 0025 可以自定义一个插值器 ，返回和网页上相同的数值变化率
         // 用Android实现的这几个长用的插值器效果不行
-//        mValueAnimator.setInterpolator(new AccelerateInterpolator(2));
+        // 默认插值器DecelerateInterpolator
+        mValueAnimator.setInterpolator(new DecelerateInterpolator());
         mValueAnimator.setDuration(time);
         mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -264,6 +437,9 @@ public class BublesSwitchView extends View {
                 animIsStart = false;
                 moveX = -1;
                 select = !select;
+                if (mOnCheckedChangeListener != null) {
+                    mOnCheckedChangeListener.onCheckedChange(BublesSwitchView.this, select);
+                }
             }
 
             @Override
@@ -279,13 +455,19 @@ public class BublesSwitchView extends View {
 
     }
 
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (mValueAnimator != null && mInterpolator != null) {
+            mValueAnimator.setInterpolator(mInterpolator);
+        }
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                event.getX();
                 return true;
             case MotionEvent.ACTION_MOVE:
 
@@ -295,13 +477,31 @@ public class BublesSwitchView extends View {
                 if (!animIsStart) {
                     animIsStart = true;
                     invalidate();
+                    if (mOnClickListener != null) {
+                        mOnClickListener.onClick(this);
+                    }
                 }
                 break;
         }
-
         return super.onTouchEvent(event);
     }
 
+
+    private class MyDecelerateInterpolator implements Interpolator {
+
+        float mFactor = 0.1f;
+
+        @Override
+        public float getInterpolation(float input) {
+            float result;
+            if (mFactor == 1.0f) {
+                result = (float) (1.0f - Math.pow((1.0f - input), 2 * mFactor));
+            } else {
+                result = (float) (1.0f - (1.0f - input) * (1.0f - input));
+            }
+            return result;
+        }
+    }
 
     //=====================================================================================================
     //历史版本
